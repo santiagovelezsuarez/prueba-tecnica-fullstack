46428b54f5a9e6fca174c602cf537a65d9044a1c
Santiago Velez <santiago.velez@autoidentificacion.net>
2025-03-31 13:23:04 -0500

    ajustes repo

--- lib/utils.ts
+++ lib/utils.ts
@@ -59,9 +59,8 @@ export const fetchGraphQL = async (query: string, variables: Record<string, any>
           headers,
           credentials: "include",          
           body: JSON.stringify({ query, variables })          
-      });
-      const data = await res.json();     
-
+      });      
+      const data = await res.json();      
       if (data.errors) {        
           throw new Error(data.errors[0].message);
       }
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,25 +1,28 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import resolvers from '@/graphql/resolvers';
-import typeDefs from '@/graphql/schema';
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import resolvers from "@/graphql/resolvers";
+import typeDefs from "@/graphql/schema";
 import { authOptions } from "./auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 import { NextApiRequest, NextApiResponse } from "next";
+import nextCors from "nextjs-cors"; // 📌 Importa nextjs-cors
 
 export const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const server = new ApolloServer({ schema });
 
-export default startServerAndCreateNextHandler(server, {
-  context: async (req: NextApiRequest, res: NextApiResponse) => {    
-    
-    const session = await getServerSession(req, res, authOptions);   
+const handler = startServerAndCreateNextHandler(server, {
+  context: async (req: NextApiRequest, res: NextApiResponse) => {
+    await nextCors(req, res, {
+      origin: "*",
+      methods: ["GET", "POST", "OPTIONS"],
+      credentials: true,
+    });
 
-    return {
-      req,
-      res,
-      session
-    };
+    const session = await getServerSession(req, res, authOptions);
+    return { req, res, session };
   },
 });
+
+export default handler;
